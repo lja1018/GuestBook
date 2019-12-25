9e4c039479a5e85d68320e06c96f0ef4bdac45b0
Jae-Eon, Lim <imjaeeon@naver.com>
2019-12-25 15:31:27 +0900

    DAO

--- GuestBook/src/dao/GuestBookDao.java
+++ GuestBook/src/dao/GuestBookDao.java
@@ -9,20 +9,23 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import util.DBConnectionPool;
 import vo.GuestBook;
 
 public class GuestBookDao {
-	Connection connection;
+	DBConnectionPool connPool;
 	
-	public void setConnection(Connection connection) {
-		this.connection = connection;
+	public void setDbConnectionPool(DBConnectionPool connPool) {
+		this.connPool = connPool;
 	}
 
 	public List<GuestBook> selectList() throws Exception {
+		Connection connection = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		
 		try {
+			connection = connPool.getConnection();
 			stmt = connection.createStatement();
 			rs = stmt.executeQuery(
 					"select *" + 
@@ -49,13 +52,16 @@ public class GuestBookDao {
 		} finally {
 			try {if (rs != null) rs.close();} catch (Exception e) {}
 			try {if (stmt != null) stmt.close();} catch (Exception e) {}
+			if (connection != null) connPool.returnConnection(connection);
 		}
 	}
 	
 	public int insert(GuestBook guestbook) throws Exception {
+		Connection connection = null;
 		PreparedStatement stmt = null;
 		
 		try {
+			connection = connPool.getConnection();
 			stmt = connection.prepareStatement(
 					"insert into guestbooks(email, pwd, contents, create_date, modify_date)" + 
 					" values (?, ?, ?, now(), now())" );
@@ -71,69 +77,77 @@ public class GuestBookDao {
 			
 		} finally {
 			try {if (stmt != null) stmt.close();} catch (Exception e) {}
+			if (connection != null) connPool.returnConnection(connection);
 		}
 	}
 	
 	public int delete(int no) throws Exception {
+		Connection connection = null;
 		Statement stmt = null;
 		
 		try {
+			connection = connPool.getConnection();
 			stmt = connection.createStatement();
 			return stmt.executeUpdate("delete from guestbooks where gno=" + no);
 		
 		} catch (Exception e) {
 			throw e;
 			
+		} finally {
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			if (connection != null) connPool.returnConnection(connection);
 		}
 	}
 	
 	public int update(GuestBook guestbook) throws Exception {
+		Connection connection = null;
 		PreparedStatement stmt = null;
 		
 	    try {
-	      stmt = connection.prepareStatement(
-	          "update guestbooks set contents=?, modify_date=now()"
-	              + " where gno=?");
-	      stmt.setString(1, guestbook.getContents());
-	      stmt.setString(2, Integer.toString(guestbook.getNo()));
+	    	connection = connPool.getConnection();
+	    	stmt = connection.prepareStatement("update guestbooks set contents=?, modify_date=now() where gno=?");
+	    	stmt.setString(1, guestbook.getContents());
+	    	stmt.setString(2, Integer.toString(guestbook.getNo()));
 	      
 	      return stmt.executeUpdate();
 
 	    } catch (Exception e) {
-	      throw e;
+	    	throw e;
 
 	    } finally {
-	      try {if (stmt != null) stmt.close();} catch (Exception e) {}
+	    	try {if (stmt != null) stmt.close();} catch (Exception e) {}
+	    	if (connection != null) connPool.returnConnection(connection);
 	    }
 	}
 	
 	public GuestBook selectOne(int no) throws Exception {
+		Connection connection = null;
 		Statement stmt = null;
 	    ResultSet rs = null;
 	    
 	    try {
-	      stmt = connection.createStatement();
-	      rs = stmt.executeQuery(
-	          "select email, contents, modify_date, gno from guestbooks" + 
-	              " where gno=" + no);
-	      
-	      if (rs.next()) {
-	        return new GuestBook()
-	        		.setNo(rs.getInt("gno"))
-	        		.setEmail(rs.getString("email"))
-	        		.setContents(rs.getString("contents"))
-	        		.setCreatedDate(rs.getDate("modify_date"));
-
-	      } else {
-	        throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");
-	      }
-
+	    	connection = connPool.getConnection();
+	    	stmt = connection.createStatement();
+	    	rs = stmt.executeQuery("select email, contents, modify_date, gno from guestbooks where gno=" + no);
+	    	
+	    	if (rs.next()) {
+	    		return new GuestBook()
+	    				.setNo(rs.getInt("gno"))
+	    				.setEmail(rs.getString("email"))
+	    				.setContents(rs.getString("contents"))
+	    				.setCreatedDate(rs.getDate("modify_date"));
+	    		
+	    	} else {
+	    		throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");	
+	    	}
+	    	
 	    } catch (Exception e) {
-	      throw e;
-	      
+	    	throw e;
+	    	
 	    } finally {
-	      try {if (rs != null) rs.close();} catch(Exception e) {}
-	      try {if (stmt != null) stmt.close();} catch(Exception e) {}
+	    	try {if (rs != null) rs.close();} catch(Exception e) {}
+	    	try {if (stmt != null) stmt.close();} catch(Exception e) {}
+	    	if (connection != null) connPool.returnConnection(connection);
 	    }
 	}
 }
