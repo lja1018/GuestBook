9f8db3af11b9f66d0ed908a7df312428ece5cf57
Jae-Eon, Lim <imjaeeon@naver.com>
2019-12-26 12:09:33 +0900

    DataSource 적용

--- GuestBook/src/listeners/ContextLoaderListener.java
+++ GuestBook/src/listeners/ContextLoaderListener.java
@@ -1,16 +1,16 @@
 package listeners;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 import dao.GuestBookDao;
-import util.DBConnectionPool;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
-	DBConnectionPool connPool;
 	
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
@@ -18,14 +18,11 @@ public class ContextLoaderListener implements ServletContextListener {
 		try {
 			ServletContext sc = event.getServletContext();
 			
-			connPool = new DBConnectionPool(
-					sc.getInitParameter("driver"),
-					sc.getInitParameter("url"),
-					sc.getInitParameter("username"),
-					sc.getInitParameter("password") );
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/studydb");
 			
 			GuestBookDao guestbookDao = new GuestBookDao();
-			guestbookDao.setDbConnectionPool(connPool);
+			guestbookDao.setDataSource(ds);
 			
 			sc.setAttribute("guestbookDao", guestbookDao);
 			
@@ -36,8 +33,6 @@ public class ContextLoaderListener implements ServletContextListener {
 	}
 	
 	@Override
-	public void contextDestroyed(ServletContextEvent event) {
-		connPool.closeAll();
-	}
+	public void contextDestroyed(ServletContextEvent event) {}
 
 }
